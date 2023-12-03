01840ab369a2438f8a57663181f952d23178f255
kavishkamk <kavishkamadhushan727@gmail.com>
2023-12-03 18:56:49 +0530

    add category pages

--- ecom_app/urls.py
+++ ecom_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index-page")
+    path("", views.index, name="index-page"),
+    path("shop", views.category_preview, name="category-preview-page"),
+    path("shop/<str:category_id>", views.catetory, name="categoy-page")
 ]
--- ecom_app/views.py
+++ ecom_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def index(request):
     return render(request, "ecom_app/index.html")
+
+def catetory(request, category_id):
+    return render(request, "ecom_app/category.html")
+
+def category_preview(request):
+    return render(request, "ecom_app/category-preview.html")
