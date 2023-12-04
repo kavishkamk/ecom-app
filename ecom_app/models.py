305b3171206a83937efa49bf1c901bcdadbc2a88
kavishkamk <kavishkamadhushan727@gmail.com>
2023-12-04 11:21:40 +0530

    add view

--- ecom_app/models.py
+++ ecom_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    imageUrl = models.URLField()
+
+    def __str__(self):
+        return f"{self.title} ({self.id})"
